5373fef7c084b2853d2bf8b755426927de31c9f6
Ryan Park <ryan.park@guildeducation.com>
2018-11-05 10:01:54 -0700

    Initial setup.py with some requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='aws_jumpcloud',
+    packages=find_packages(exclude=["tests"]),
+    zip_safe=False,
+    python_requires=">=3.6",
+    install_requires=["requests", "BeautifulSoup4", "lxml", "boto3", "keyring"],
+    tests_require=["pycodestyle", "pylint"]
+)
