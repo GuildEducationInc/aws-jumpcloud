b207f11fabbdfd7e71ac9c0ebb65ead5bfc0d93f
Ryan Park <ryan.park@guildeducation.com>
2018-12-31 12:08:48 -0700

    Refactor cli._build_parser() into smaller functions

--- aws_jumpcloud/cli.py
+++ aws_jumpcloud/cli.py
@@ -23,23 +23,40 @@ def main():
 def _build_parser():
     parser = ArgumentParser(description=DESCRIPTION)
     parser.add_argument("--version", action='version', version="%(prog)s ("+__VERSION__+")")
-
     subparsers = parser.add_subparsers(dest="command")
+    _add_help_command(subparsers)
+    _add_info_command(subparsers)
+    _add_list_command(subparsers)
+    _add_add_command(subparsers)
+    _add_remove_command(subparsers)
+    _add_exec_command(subparsers)
+    _add_rotate_command(subparsers)
+    return parser
 
-    parser_help = subparsers.add_parser("help", help="show this help message and exit")
+
+def _add_help_command(p):
+    parser_help = p.add_parser("help", help="show this help message and exit")
     parser_help.set_defaults(func=_print_help)
 
-    parser_info = subparsers.add_parser("info", help="display info about your JumpCloud account")
+
+def _add_info_command(p):
+    parser_info = p.add_parser("info", help="display info about your JumpCloud account")
     parser_info.set_defaults(func=commands.get_info)
 
-    parser_list = subparsers.add_parser("list", help="list profiles and their sessions")
+
+def _add_list_command(p):
+    parser_list = p.add_parser("list", help="list profiles and their sessions")
     parser_list.set_defaults(func=commands.list_profiles)
 
-    parser_add = subparsers.add_parser("add", help="add a new profile")
+
+def _add_add_command(p):
+    parser_add = p.add_parser("add", help="add a new profile")
     parser_add.add_argument("profile", help="name of the profile")
     parser_add.set_defaults(func=commands.add_profile)
 
-    parser_remove = subparsers.add_parser("remove", help="remove a profile and any temporary IAM sessions")
+
+def _add_remove_command(p):
+    parser_remove = p.add_parser("remove", help="remove a profile and any temporary IAM sessions")
     parser_remove_mx = parser_remove.add_mutually_exclusive_group(required=True)
     parser_remove_mx.add_argument("profile", help="name of the profile", nargs="?")
     parser_remove_mx.add_argument(
@@ -47,13 +64,17 @@ def _build_parser():
         help="revoke all temporary IAM sessions and deletes stored JumpCloud authentication information.")
     parser_remove.set_defaults(func=commands.remove_profile)
 
-    parser_exec = subparsers.add_parser(
+
+def _add_exec_command(p):
+    parser_exec = p.add_parser(
         "exec", help="executes a command with AWS credentials in the environment")
     parser_exec.add_argument("profile", help="name of the profile")
     parser_exec.add_argument("command", nargs="+")
     parser_exec.set_defaults(func=commands.exec_command)
 
-    parser_rotate = subparsers.add_parser(
+
+def _add_rotate_command(p):
+    parser_rotate = p.add_parser(
         "rotate",
         help="rotates the IAM session (generates new temporary credentials) for an existing profile")
     parser_rotate_mx = parser_rotate.add_mutually_exclusive_group(required=True)
@@ -62,8 +83,6 @@ def _build_parser():
         "--all", action="store_true",
         help="generate new temporary IAM credentials for all existing profiles")
     parser_rotate.set_defaults(func=commands.rotate_session)
-
-    return parser
 
 
 def _print_help(args):
