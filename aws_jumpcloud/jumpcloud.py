c81357dced2ebe4fd3b3fd58793d696102f83ff7
Ryan Park <ryan.park@guildeducation.com>
2018-12-31 11:59:58 -0700

    Reduce complexity of JumpCloudSession._auth_failure_exception

--- aws_jumpcloud/jumpcloud.py
+++ aws_jumpcloud/jumpcloud.py
@@ -48,15 +48,18 @@ class JumpCloudSession(object):
 
     def _auth_failure_exception(self, auth_resp, otp):
         assert(auth_resp.status_code != 200)
+        try:
+            error_msg = auth_resp.json().get("error", "")
+        except JSONDecodeError:
+            error_msg = ""
+
         if otp is None and auth_resp.status_code == 302 and "error=4014" in auth_resp.headers['Location']:
             return JumpCloudMFARequired(auth_resp)
         elif auth_resp.status_code == 401:
-            try:
-                if otp is not None and "multifactor" in auth_resp.json().get("error"):
-                    return JumpCloudMFAFailure(auth_resp)
-            except JSONDecodeError:
-                pass
-            return JumpCloudAuthFailure(auth_resp)
+            if otp is not None and "multifactor" in error_msg:
+                return JumpCloudMFAFailure(auth_resp)
+            else:
+                return JumpCloudAuthFailure(auth_resp)
         elif auth_resp.status_code > 500:
             return JumpCloudServerError(auth_resp)
         else:
