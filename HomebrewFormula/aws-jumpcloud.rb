a7346651181e0b126785835a8669dc47e3487169
Ryan Park <ryan.park@guildeducation.com>
2019-01-08 15:23:45 -0700

    Update Homebrew formula to download version 1.2.0

--- HomebrewFormula/aws-jumpcloud.rb
+++ HomebrewFormula/aws-jumpcloud.rb
@@ -3,8 +3,8 @@ class AwsJumpcloud < Formula
 
   desc "aws-vault like tool for JumpCloud authentication"
   homepage "https://github.com/GuildEducationInc/aws-jumpcloud"
-  url "https://github.com/GuildEducationInc/aws-jumpcloud/archive/1.1.2.tar.gz"
-  sha256 "6fc90aee8f705ea9ab43e6aa59b0c64d1d82ad9635915073ac1f94d9250d314b"
+  url "https://github.com/GuildEducationInc/aws-jumpcloud/archive/1.2.0.tar.gz"
+  sha256 "3d3a0ecdd42054200b343e50d698b938caf1fbc61b606ae8ee16804f320adfe0"
   head "https://github.com/GuildEducationInc/aws-jumpcloud.git", :branch => 'master'
   depends_on "python"
 
