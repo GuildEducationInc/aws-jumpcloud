5debe1c91dfaa17cbb56c9999558594d278cba83
Jon Peterson <jon.peterson@guildeducation.com>
2020-01-31 15:03:53 -0700

    Enhance usage of op CLI

--- aws_jumpcloud/onepassword.py
+++ aws_jumpcloud/onepassword.py
@@ -1,16 +1,28 @@
 from shutil import which
+import sys
 import os
+import subprocess
 import json
 
 ITEM = "jumpcloud"
 
 
 def installed():
-    return which("op") is not None
+    hasop = which("op") is not None
+    if hasop:
+        if os.getenv('OP_SUBDOMAIN') and subprocess.call("op get account", shell=True):  # returns truthy non-zero exit code if no active op CLI session
+            subprocess.check_call("eval $(op signin $OP_SUBDOMAIN)", shell=True)
+        if not os.getenv('OP_SUBDOMAIN'):
+            raise OnePasswordNotSignedIn()
+    return hasop
 
 
 def get_email():
-    return _get_field("email")
+    email = _get_field("email")
+    if not email:
+        email = _get_field("username")
+    # if no email at this point, raise?
+    return email
 
 
 def get_password():
@@ -24,7 +36,7 @@ def _get_field(field):
         return None
 
     fields = item['details']['fields']
-    return next(item.get('value') for item in fields if item.get('name') == field)
+    return next((item.get('value') for item in fields if item.get('name') == field), None)
 
 
 def _get_item():
@@ -37,8 +49,20 @@ def _get_item():
 
 
 def get_totp():
-    return _cmd("get totp")
+    return _cmd(f"get totp {ITEM}")
 
 
 def _cmd(cmd):
-    return os.popen(f'op {cmd} {ITEM}').read().strip()
+    return subprocess.check_output(f'op {cmd}', shell=True).decode(sys.stdout.encoding).strip()
+
+class OnePasswordNotSignedIn(Exception):
+    def __init__(self):
+        message = """
+            1Password CLI is not signed in. Either export your 1Password subdomain (e.g. duff-beer):
+                export OP_SUBDOMAIN=duff-beer
+            or manually sign in with:
+                eval $(op signin duff-beer)
+            and try again.
+        """
+        Exception.__init(self, message)
+        self.message = message
